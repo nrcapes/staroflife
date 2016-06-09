410499c1eb87d9742fec8b637a2b49b5d037c868
nrcapes <nrcapes@gmail.com>
2016-06-09 14:20:19 -0500

    changes needed to make Swift code interact with objective-c.

---
+++ NRCLogin.swift
@@ -0,0 +1,13 @@
+//
+//  NRCLogin.swift
+//  EMS Timers Professional
+//
+//  Created by Nelson Capes on 6/8/16.
+//  Copyright © 2016 Nelson Capes. All rights reserved.
+//
+
+import UIKit
+
+class NRCLogin: NSObject {
+
+}
---
+++ NRCLoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NRCLoginViewController.swift
+//  EMS Timers Professional
+//
+//  Created by Nelson Capes on 6/8/16.
+//  Copyright © 2016 Nelson Capes. All rights reserved.
+//
+
+import UIKit
+
+class NRCLoginViewController: NSObject {
+
+}
