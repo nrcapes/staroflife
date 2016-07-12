161ea255c3460ecad1562696134423bebe1a8cd6
nrcapes <nrcapes@gmail.com>
2016-07-12 13:53:51 -0500

    add [super viewWillAppear] to purchase view controller after Analyze run.

--- EMSTimers/PurchaseViewController.m
+++ EMSTimers/PurchaseViewController.m
@@ -41,6 +41,7 @@
 
 -(void)viewWillAppear:(BOOL)animated{
     [self getProductInfo];
+    [super viewWillAppear:animated];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
