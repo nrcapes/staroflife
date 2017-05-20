fefc70160324fd41906f4ee699c859e5ec946295
nrcapes <nrcapes@gmail.com>
2017-05-20 10:59:51 -0500

    added lauchscreen.storyboard image.  Added required Apple information in splash screen.

--- NRCStoreKitViewController.m
+++ NRCStoreKitViewController.m
@@ -267,7 +267,7 @@ static NSUInteger const kProductPurchasedAlertViewTag = 1;
     title = [title stringByAppendingString:@". Payment will be charged to iTunesAccount at confirmation of purchase. Subscription automatically renews unless auto-renew is turned off at least 24-hours before the end of the current period. Account will be charged for renewal within 24-hours prior to the end of the current period, and the cost of renewal is:"];
     title = [title stringByAppendingString:@""];
     title = [title stringByAppendingString:self.userSelectedProductLocalizedPrice];
-    title = [title stringByAppendingString:@" .Subscriptions may be managed by the user and auto-renewal may be turned off by going to the user's Account Settings after purchase. Privacy policy and terms of use at: www.starpointsoftware.com."];
+    title = [title stringByAppendingString:@" .Subscriptions may be managed by the user and auto-renewal may be turned off by going to the user's Account Settings after purchase. Privacy policy is at: www.starpointsoftware.com. Terms of Use are governed by the Apple Licensed Application End User License Agreement."];
     title = [title stringByAppendingString:@" Any unused portion of a free trial period, if offered, will be forfeited when the user purchases a subscription to that publication, where appropriate."];
     
     UIAlertController * alert=   [UIAlertController
