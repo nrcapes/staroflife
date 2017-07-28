689c0888ac9f9e58d3c6d0fc1fe2be96c8e330e7
nrcapes <nrcapes@gmail.com>
2017-07-28 13:17:05 -0500

    resurrected app under staroflife.

---
+++ Star of Life.xcodeproj/PurchaseViewController.h
@@ -0,0 +1,38 @@
+//
+//  PurchaseViewController.h
+//  Timechime
+//
+//  Created by Nelson Capes on 3/30/17.
+//  Copyright © 2016 Nelson Capes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <StoreKit/StoreKit.h>
+
+
+//@class NRCTableViewController;
+@interface PurchaseViewController :  UIViewController <SKPaymentTransactionObserver, SKProductsRequestDelegate>
+@property (strong) NSString *productToBuy;
+@property (strong, nonatomic) SKProduct *product;
+@property (strong, nonatomic) NSString *productID;
+@property (strong, nonatomic) SKPaymentTransaction *transaction;
+@property (strong, nonatomic) SKPayment *payment;
+//@property (strong, nonatomic) SKPayment *payment2;
+@property (strong, nonatomic) IBOutlet UILabel *productTitle;
+@property (strong, nonatomic) IBOutlet UIButton *buyButton;
+@property (strong, nonatomic) IBOutlet UITextView *productDescription;
+@property (strong) NSString *productToUnlock;
+@property BOOL centralAdminUnlocked;
+@property BOOL iCloudSupportUnlocked;
+@property BOOL productAvailable;
+@property (strong, nonatomic) NSSet *requestSet;
+@property (strong, nonatomic) SKProductsRequest *request;
+@property (strong, nonatomic) NSArray *products;
+@property (strong, nonatomic) NSDate *transactionDate;
+@property (strong, nonatomic) NSMutableDictionary *productDict;
+@property (strong, nonatomic) NSMutableDictionary *transactionDates;
+//@property(strong,nonatomic)NRCTableViewController *homeViewController;
+- (IBAction)buyProduct:(id)sender;
+- (void)getProductInfo;
+-(IBAction)restoreCompletedTransactions:(id)sender;
+@end
